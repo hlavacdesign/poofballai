a9ec3aed0515423c63c395d5b1909ff898b5f4e7
Michal Hlaváč <michal@hlavac.design>
2024-12-17 17:19:29 -0800

    added production IP address

--- python/server/server.py
+++ python/server/server.py
@@ -42,4 +42,6 @@ def chat():
 
 if __name__ == '__main__':
     # Run the server locally
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    # app.run(host='127.0.0.1', port=5000, debug=True)
+    # For deployment
+    app.run(host='0.0.0.0', port=5000)
